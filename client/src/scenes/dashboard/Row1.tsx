8047ee82994c031ee6e5582b5fe12e6e32bb7260
unknown <firas@bentaleb>
2024-02-28 17:32:47 +0100

    areachart dashbord

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,12 +1,52 @@
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { useMemo } from "react";
 
 
 const Row1 = () => {
   const {data} = useGetKpisQuery();
+  const { palette } = useTheme();
+
+  console.log('data',data)
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        };
+      })
+    );
+  }, [data]);
+
+
+
   return (
     <>
-    <DashboardBox gridArea="a"></DashboardBox>
+    <DashboardBox gridArea="a">
+    <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="revenue" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </DashboardBox>
     <DashboardBox gridArea="b"></DashboardBox>
     <DashboardBox gridArea="c"></DashboardBox>
     </>
